8b67bb4a36cf5ff7d6ea3228dbdafa7c3806f115
Felix <felix.lahemade@gmail.com>
2015-06-06 00:09:23 +0200

    Improvement of GUI.

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -1,27 +1,53 @@
 package gui;
 
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Window extends JFrame{
-	
-	/**
+public class Window extends JFrame
+{
+    /**
 	 * 
 	 */
 	private static final long serialVersionUID = 7446192599263749847L;
-	
-		private JPanel panel = new JPanel();
-		private JButton bouton = new JButton("First Button");
+
+	public Window()
+    {
+        super("GL52 | Bus Network");                 
+        setSize(new Dimension(800,600));    
+
+        JPanel pan=new JPanel();
+        BoxLayout    bl=new BoxLayout(pan,BoxLayout.Y_AXIS);
+        pan.setLayout(bl);       
+
+        JTextField  tf=new JTextField("Editez !");
+        pan.add(tf);       
 
 
-		public Window(){
-		    this.setTitle("GL52 | BusNetwork");
-		    this.setSize(300, 150);
-		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    this.setLocationRelativeTo(null);
-		    panel.add(bouton);
-		    this.setContentPane(panel);
-		    this.setVisible(true);
-		}  
+        JPanel  pan2=new   JPanel(); 
+        bl=new BoxLayout(pan2,BoxLayout.X_AXIS); 
+        pan2.setLayout(bl);       
+
+        String  c[] ={"Add a bus","Add a stop","Add a line"};
+        JComboBox   cb=new JComboBox(c);
+        pan2.add(cb);       
+
+        pan.add(pan2);       
+
+        JButton but=new JButton("Validate");
+        pan.add(but);       
+        JButton but2=new JButton("Run");
+        pan.add(but2);  
+
+        
+        setContentPane(pan);        
+        setVisible(true);    
+        
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }   
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
 import gui.Window;
+import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] args){
-		Window window = new Window();
-	}
+	
+	  public static void main(String[] args) throws InterruptedException{
+	    JFrame window = new Window();
+	    
+	    for(int i=0;i<1000000;i++){
+	    	System.out.println("Running");
+	    }
+	    window.dispose();
+	    
+	  }      
 }
