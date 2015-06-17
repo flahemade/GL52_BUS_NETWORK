207db67fc2f55621a1e0f36cb13f717af2c369f5
Felix <felix.lahemade@gmail.com>
2015-06-18 00:03:38 +0200

    Improved GUI.

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -3,8 +3,8 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
@@ -14,19 +14,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import environment.Bus;
+import environment.BusStation;
+import environment.Network;
+import environment.Position;
 import environment.RoadSegment;
 
-public class Window extends JFrame implements ActionListener
+public class Window extends JFrame implements MouseListener
 {
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 7446192599263749847L;
 	
-	private JButton but1,but2,but3,imageButton;
-	JPanel pan;
-	ArrayList<RoadSegment> rs;
-	ArrayList<Bus> bl;
+	private JButton but1,but2,but3,but4,imageButton;
+	private JPanel pan;
+	private Network n;
+	private boolean segmentSource;
+	private boolean busStop;
+	private Position currentSource;
 
 	public Window()
     {
@@ -37,13 +42,14 @@ public class Window extends JFrame implements ActionListener
         BoxLayout bl=new BoxLayout(pan,BoxLayout.Y_AXIS);
         pan.setLayout(bl);       
 
-
+        segmentSource=false;
+        busStop=false;
 
 		ImageIcon image = new ImageIcon("res/output.png");
 		imageButton = new JButton("",image);
         pan.add( imageButton, BorderLayout.CENTER );   
 
-        imageButton.addActionListener(this);
+        imageButton.addMouseListener(this);
 
         JPanel  pan2=new   JPanel(); 
         bl=new BoxLayout(pan2,BoxLayout.X_AXIS); 
@@ -53,14 +59,17 @@ public class Window extends JFrame implements ActionListener
         pan.add(but1);      
         but2=new JButton("Add a stop");
         pan.add(but2);
+        but4=new JButton("Finish");
+        pan.add(but4);  
         but3=new JButton("Run");
         pan.add(but3);  
 
         //Listeners
         
-        but1.addActionListener(this);
-        but2.addActionListener(this);
-        but3.addActionListener(this);
+        but1.addMouseListener(this);
+        but2.addMouseListener(this);
+        but3.addMouseListener(this);
+        but4.addMouseListener(this);
         
         setContentPane(pan);        
         setVisible(true);    
@@ -68,29 +77,74 @@ public class Window extends JFrame implements ActionListener
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+	public void draw(ArrayList<RoadSegment> rs){
+		Graphics g = pan.getGraphics();
+		for (RoadSegment r : rs) {
+			g.drawLine(
+					r.getStart().getX(), 
+					r.getStart().getY(), 
+					r.getEnd().getX(), 
+					r.getEnd().getX()
+					);
+		}
+		for (Bus b : n.getBus()){
+			g.drawRect(
+					b.getRoadSegment().getStart().getX(), 
+					b.getRoadSegment().getStart().getY(), 
+					10, 
+					5);
+		}
+	}
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void mouseClicked(MouseEvent e) {
 		Object source = e.getSource();
 		if(source == but1){
-			System.out.println("Click sur le bouton \"Add a line\"");
+			if(!segmentSource){
+				segmentSource = true;
+			}
 		}else if(source == but2){
-			System.out.println("Click sur le bouton \"Add a stop\"");
-			//setStop=true;
+			if(!busStop){
+				busStop=true;
+			}
 		}else if(source == but3){
 			System.out.println("Click sur le bouton \"Run\"");
 			//run();
+		}else if(source == but4){
+			System.out.println("Click sur le bouton \"Finish\"");
+			if(segmentSource)segmentSource=false;
+			if(busStop)busStop=false;
 		}else if(source == imageButton){
-			System.out.println("Click sur l'image.");
+			if(segmentSource){
+				m.addRoadSegment(currentSource,new Position(e.getX(),e.getY()));
+			}else if(busStop){
+				m.addBusStop(Position p, String name);
+			}
 		}
-	}  
-	
-	public void draw(ArrayList<RoadSegment> rs, ArrayList<Bus> bl){
-		Graphics g = pan.getGraphics();
-		for (RoadSegment r : rs) {
-			g.drawLine(r.getStart().getX(), r.getStart().getY(), r.getEnd().getX(), r.getEnd().getX());
-		}
-		for (Bus b : bl){
-		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
